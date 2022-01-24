80bffba0dd30130d4b9a809efc40efcfad0830d7
Keenan Matrix <keenanmatrixpt@gmail.com>
2022-01-24 13:55:05 -0500

    Playing around with 3D modelfor NFT's

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import * as THREE from 'three'
+import {useEffect, useLayoutEffect} from 'react'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import OBJ from './head5.obj'
+import MTL from './head5.mtl'
+
+const objLoader = new OBJLoader()
+const scene = new THREE.Scene()
+let Beet
+const mtlLoader = new MTLLoader()
+mtlLoader.load(
+  MTL,
+  (materials) => {
+    materials.preload()
+
+    const objLoader = new OBJLoader()
+    objLoader.setMaterials(materials)
+    objLoader.load(
+      OBJ,
+      (object) => {
+        Beet = object
+        scene.add(Beet)
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+        console.log('An error happened')
+      },
+    )
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {
+    console.log('An error happened')
+  },
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useLayoutEffect(() => {
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    )
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.getElementById('myCanvas'),
+    })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.position.setZ(20)
+    renderer.render(scene, camera)
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xff6347,
+      wireframe: true,
+    })
+    const torus = new THREE.Mesh(geometry, material)
+    //scene.add(torus)
+
+    const pointLight = new THREE.PointLight(0xb85b14)
+    const pointLight2 = new THREE.PointLight(0xb85b14)
+    pointLight.position.set(-5, 5, 10)
+    pointLight2.position.set(30, 5, -50)
+    //const ambientLight = new THREE.AmbientLight(0xe6e6e6)
+    //scene.add(ambientLight)
+    scene.add(pointLight)
+    scene.add(pointLight2)
+    //const gridHelper = new THREE.GridHelper(200, 50)
+    const lightHelper = new THREE.PointLightHelper(pointLight)
+    //scene.add(lightHelper)
+    const lightHelper2 = new THREE.PointLightHelper(pointLight2)
+    //scene.add(lightHelper2)
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    function animate() {
+      requestAnimationFrame(animate)
+      try {
+        Beet.rotation.y += 0.005
+      } catch (error) {}
+
+      controls.update()
+      renderer.render(scene, camera)
+    }
+
+    animate()
+  })
+  useEffect(() => {}, [])
+  return <canvas id="myCanvas" position="fixed" top="0" left="0"></canvas>
 }
 
-export default App;
+export default App
